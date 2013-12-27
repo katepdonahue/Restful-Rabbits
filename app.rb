adf6756ad444c41dc1f10242336b1d50494af0f0
Kate <katepdonahue@gmail.com>
2013-12-27 14:57:08 -0500

    Add first route and index view

--- app.rb
+++ app.rb
@@ -3,14 +3,11 @@ Bundler.require
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rabbits.db")
 
-class Rabbit
-  include DataMapper::Resource
-  property :id,           Serial
-  property :name,         String, :required => true
-  property :description,  Text
-  property :age,          Integer
-  property :color,        String
-  property :created_at,   DateTime
-  property :updated_at,   DateTime
+class App
+
+  get '/rabbits' do
+    @rabbits = Rabbit.all
+    haml :index
+  end
 
 end
