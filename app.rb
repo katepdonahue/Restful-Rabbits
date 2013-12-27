903db52c3f947b414b7da481ce12ea014e79f0df
Kate <katepdonahue@gmail.com>
2013-12-27 14:48:02 -0500

    Set up the attributes that will be the columns in the database

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ Bundler.require
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rabbits.db")
 
 class Rabbit
-  
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :description,  Text
+  property :age,          Integer
+  property :color,        String
+  property :created_at,   DateTime
+  property :updated_at,   DateTime
+
 end
