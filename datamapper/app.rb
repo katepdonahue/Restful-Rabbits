b7da63d8826cd0284ad48093e64433a79d72ea9f
Kate <katepdonahue@gmail.com>
2013-12-27 19:22:37 -0500

    Make App class inherit from sinatra application

--- datamapper/app.rb
+++ datamapper/app.rb
@@ -4,7 +4,7 @@ require './lib/rabbits'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rabbits.db")
 
-class App
+class App < Sinatra::Application
 
   get '/rabbits' do
     @rabbits = Rabbit.all
